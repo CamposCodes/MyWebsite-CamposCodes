8eed1e14f7f36b8ba88f7b720740e8fe35400e08
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2023-04-25 13:17:46 -0300

    Update my portifólio web site

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,126 +1,142 @@
-import * as C from './styles';
-import Theme from '../../components/Theme';
+import * as C from "./styles";
+import Theme from "../../components/Theme";
 
-import profile from '../../assets/profile.png';
+import profile from "../../assets/profile.png";
 
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 
-import { DiReact,DiJsBadge,DiGitBranch } from "react-icons/di";
+import { DiReact, DiJsBadge, DiGitBranch } from "react-icons/di";
 import { SiTypescript } from "react-icons/si";
 
-
-
 const Home = () => {
-
-
-    return (
-        <Theme>
-        <motion.div
-            style={{width:100}}
-            onViewportEnter={()=>window.scrollTo(0, 0)}
-            initial={{width: 0}}
-            animate={{width: "100%"}}
-            exit={{x: window.innerWidth, transition:{duration: 0.5}}}
-        >
-            <C.Container>
-                <C.TextArea>
-                    <C.Image src={profile}></C.Image>
-                    <C.Text>
-                        <C.Title>About me</C.Title>
-                        I am Gabriel Campos and a I.T. student who is passionate about writing code, solving problems, and automating stuff, and i'm always looking for improvement and new knowledge to build better solutions through apps.
-                    </C.Text>
-                </C.TextArea>
-                <C.TextArea2>
-                    <C.Text2>
-                        <C.SubTitle>technologies I work with</C.SubTitle>
-                    </C.Text2>
-                </C.TextArea2>
-                <C.ButtonContainer>
-                    <motion.div
-                    initial={{ scale: 0 }}
-                    animate={{ scale: [1,1.3,1.5,1.2], rotateX:[0,20,-20,0]}}
-                    transition={{
-                        type: "spring",
-                        stiffness: 50,
-                        damping: 20,
-                        delay:.2,
-                        duration: 2
-                    }}>
-                        <C.LinkButton to="/projects">
-                            <DiReact className="icon1"/>
-                        </C.LinkButton> 
-                    </motion.div>
-                    <motion.div
-                    initial={{ scale: 0 }}
-                    animate={{ scale: [1,1.3,1.5,1], rotateX:[0,20,-20,0]}}
-                    transition={{
-                        type: "spring",
-                        stiffness: 50,
-                        damping: 20,
-                        delay:2.2,
-                        duration: 2
-                    }}> 
-                        <C.LinkButton to="/contact">
-                            <DiJsBadge className="icon2"/>
-                        </C.LinkButton> 
-                    </motion.div>
-                    <motion.div  
-                    initial={{ scale: 0 }}
-                    animate={{ scale: [1,1.3,1.5,1.2], rotateX:[0,20,-20,0]}}
-                    transition={{
-                        type: "spring",
-                        stiffness: 50,
-                        damping: 20,
-                        delay:4.2,
-                        duration: 2
-                    }}> 
-                        <C.LinkButton to="/about">
-                            <DiGitBranch className="icon1"/>
-                        </C.LinkButton> 
-                    </motion.div>
-                    <motion.div
-                    initial={{ scale: 0 }}
-                    animate={{ scale: [1,1.3,1.5,1], rotateX:[0,20,-20,0]}}
-                    transition={{
-                        type: "spring",
-                        stiffness: 50,
-                        damping: 20,
-                        delay:6.2,
-                        duration: 2
-                    }}> 
-                        <C.LinkButton to="/about">
-                            <SiTypescript className="icon2"/>
-                        </C.LinkButton> 
-                    </motion.div>
-                </C.ButtonContainer>
-                <C.TextArea>
-                    <C.Image src={profile}></C.Image>
-                    <C.Text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </C.Text>
-                </C.TextArea>
-                <C.TextArea4>
-                    <C.Text>
-                        <C.Title2>My experiences as a developer</C.Title2>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.s
-                    </C.Text>
-                </C.TextArea4>
-                <C.TextArea3>
-                    <C.Text>
-                        <C.Title2>More</C.Title2>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </C.Text>
-                    <C.Text>
-                        <C.Title2>More</C.Title2>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.s
-                    </C.Text>
-                </C.TextArea3>
-            </C.Container>
+  return (
+    <Theme>
+      <motion.div
+        style={{ width: 100 }}
+        onViewportEnter={() => window.scrollTo(0, 0)}
+        initial={{ width: 0 }}
+        animate={{ width: "100%" }}
+        exit={{ x: window.innerWidth, transition: { duration: 0.5 } }}
+      >
+        <C.Container>
+          <C.TextArea>
+            <C.Image src={profile}></C.Image>
+            <C.Text>
+              <C.Title>Sobre mim</C.Title> Olá! Meu nome é Gabriel Campos Lima
+              Alves e atualmente estou cursando Sistemas de Informação na
+              Universidade Federal de Juiz de Fora, em Minas Gerais. Tenho 21
+              anos e estou em busca da minha primeira oportunidade no mercado de
+              trabalho como desenvolvedor front-end.
+            </C.Text>
+          </C.TextArea>
+          <C.TextArea2>
+            <C.Text2>
+              <C.SubTitle>Tecnologias que trabalho</C.SubTitle>
+            </C.Text2>
+          </C.TextArea2>
+          <C.TextArea5>
+            <C.Text>
+              Com o domínio das linguagens abaixo, tenho certeza de que posso
+              contribuir para o desenvolvimento de projetos de sucesso e agregar
+              valor à sua equipe.
+            </C.Text>
+          </C.TextArea5>
+          <C.ButtonContainer>
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: [1, 1.3, 1.5, 1.2], rotateX: [0, 20, -20, 0] }}
+              transition={{
+                type: "spring",
+                stiffness: 50,
+                damping: 20,
+                delay: 0.2,
+                duration: 2,
+              }}
+            >
+              <C.LinkButton to="/projects">
+                <DiReact className="icon1" />
+              </C.LinkButton>
             </motion.div>
-        </Theme>
-    );
-}
-
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: [1, 1.3, 1.5, 1], rotateX: [0, 20, -20, 0] }}
+              transition={{
+                type: "spring",
+                stiffness: 50,
+                damping: 20,
+                delay: 2.2,
+                duration: 2,
+              }}
+            >
+              <C.LinkButton to="/contact">
+                <DiJsBadge className="icon2" />
+              </C.LinkButton>
+            </motion.div>
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: [1, 1.3, 1.5, 1.2], rotateX: [0, 20, -20, 0] }}
+              transition={{
+                type: "spring",
+                stiffness: 50,
+                damping: 20,
+                delay: 4.2,
+                duration: 2,
+              }}
+            >
+              <C.LinkButton to="/about">
+                <DiGitBranch className="icon1" />
+              </C.LinkButton>
+            </motion.div>
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: [1, 1.3, 1.5, 1], rotateX: [0, 20, -20, 0] }}
+              transition={{
+                type: "spring",
+                stiffness: 50,
+                damping: 20,
+                delay: 6.2,
+                duration: 2,
+              }}
+            >
+              <C.LinkButton to="/about">
+                <SiTypescript className="icon2" />
+              </C.LinkButton>
+            </motion.div>
+          </C.ButtonContainer>
+          <C.TextArea4>
+            <C.Text>
+              <C.Title2>Objetivo</C.Title2>
+              Meu objetivo é criar experiências incríveis para os usuários,
+              utilizando tecnologias inovadoras e soluções criativas. Estou
+              animado para fazer parte de uma equipe dinâmica e colaborativa, e
+              contribuir para o sucesso da empresa. Acredito que minha dedicação
+              e habilidades técnicas podem ser uma grande contribuição para
+              projetos de sucesso. Mal posso esperar para mostrar todo o meu
+              potencial e crescer junto com a equipe!
+            </C.Text>
+          </C.TextArea4>
+          <C.TextArea3>
+            <C.Text>
+              <C.Title2>Motivação</C.Title2>
+              Estou muito motivado a fazer parte de uma empresa inovadora que
+              valorize o trabalho em equipe e que me proporcione um ambiente de
+              aprendizado contínuo. Acredito que minhas habilidades técnicas,
+              combinadas com minha determinação em alcançar objetivos, me tornam
+              um forte candidato para a vaga de desenvolvedor front-end.
+            </C.Text>
+            <C.Text>
+              <C.Title2>Disposição</C.Title2>
+              Fico à disposição para conversar e apresentar mais sobre minhas
+              habilidades e experiências. Desde já agradeço pela oportunidade e
+              espero fazer parte do seu time em breve. E como disse Chris Pine,
+              "Programação não é sobre o que você sabe, é sobre o que você pode
+              descobrir."
+            </C.Text>
+          </C.TextArea3>
+        </C.Container>
+      </motion.div>
+    </Theme>
+  );
+};
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,154 +1,161 @@
 import styled from "styled-components";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const ButtonContainer = styled.div`
-    min-width: 100%;
-    heigh: 100rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  min-width: 100%;
+  heigh: 100rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
-
-
 
 export const LinkButton = styled(Link)`
-    padding: 1rem;
-    border-radius: 1rem;
-    margin: .5rem;
-    text-align: center;
-    display: flex;
-    
-    &:hover{
+  padding: 1rem;
+  border-radius: 1rem;
+  margin: 0.5rem;
+  text-align: center;
+  display: flex;
+
+  &:hover {
+  }
+
+  .icon1 {
+    margin: 0 auto;
+    font-size: 2.7rem;
+    color: var(--wht);
+    transition: 0.5s;
+    filter: drop-shadow(0 0 0 var(--wht));
+    color: var(--blue1);
+
+    &:hover {
+      filter: drop-shadow(0 0 2rem var(--blue1));
+      color: var(--wht);
+      transform: scale(1.4);
     }
-    
-    .icon1{
-        margin: 0 auto;
-        font-size: 2.7rem;
-        color: var(--wht);
-        transition: .5s;
-        filter: drop-shadow(0 0 0 var(--wht));
-        color: var(--blue1);
-        
-        &:hover{
-            filter: drop-shadow(0 0 2rem  var(--blue1));
-            color: var(--wht);
-            transform: scale(1.4);
-        }
+  }
+  .icon2 {
+    margin: 0 auto;
+    font-size: 2rem;
+    color: var(--blue1);
+    transition: 0.5s;
+    filter: drop-shadow(0 0 0 var(--wht));
+
+    &:hover {
+      color: var(--wht);
+      filter: drop-shadow(0 0 3rem var(--blue1));
+      transform: scale(1.4);
     }
-    .icon2{
-        margin: 0 auto;
-        font-size: 2rem;
-        color: var(--blue1);
-        transition: .5s;
-        filter: drop-shadow(0 0 0 var(--wht));
-        
-        &:hover{
-            color: var(--wht);
-            filter: drop-shadow(0 0 3rem  var(--blue1));
-            transform: scale(1.4);
-        }
-    }
-    
+  }
 `;
 
-
 export const Container = styled.section`
-    width: 95%;
+  width: 95%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0 auto;
+  color: var(--wht);
+  position: static;
+
+  @media (max-width: 700px) {
     height: auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    margin: 0 auto;
-    color: var(--wht);
-    position: static;
-    
-    @media (max-width: 700px) {
-        height: auto;
-        width: 100%;
-    }
+    width: 100%;
+  }
 `;
 
 export const TextArea = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: auto;
-    margin: .5rem 0;
-    margin-top: 2rem;
-    background: rgba( 255, 255, 255, 0.25 );
-    box-shadow: 0 8px 32px 0 rgba( 0, 0,0, 0.37 );
-    backdrop-filter: blur( 1.5px );
-    -webkit-backdrop-filter: blur( 1.5px );
-    border-radius: 1rem;
-    `;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  margin: 0.5rem 0;
+  margin-top: 2rem;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
+  backdrop-filter: blur(1.5px);
+  -webkit-backdrop-filter: blur(1.5px);
+  border-radius: 1rem;
+`;
 export const TextArea2 = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: auto;
-    margin-top: 2rem;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  margin-top: 2rem;
 `;
 export const TextArea3 = styled.div`
-    width: 100%;
-    height: auto;
-    margin: .5rem 0;
-    margin-top: 2rem;
-    display: flex;
-    align-items: center;
-    justify-content:center;
+  width: 100%;
+  height: auto;
+  margin: 0.5rem 0;
+  margin-top: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 export const TextArea4 = styled.div`
-    width: 100%;
-    height: auto;
-    margin: .5rem 0;
-    margin-top: 2rem;
-    display: flex;
-    flex-direction:column;
-    align-items: center;
-    justify-content:center;
+  width: 100%;
+  height: auto;
+  margin: 0.5rem 0;
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const TextArea5 = styled.div`
+  width: 100%;
+  height: auto;
+  margin: 0.5rem 0;
+  margin-top: 0.1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
 `;
 
 export const Text = styled.p`
-    width: 100%;
-    text-align: start;
-    margin: 0.5rem;
-    font-size: 20px;
-    color: var(--wht);
+  width: 100%;
+  text-align: start;
+  margin: 0.5rem;
+  font-size: 20px;
+  color: var(--wht);
 `;
 export const Text2 = styled.p`
-    position: relative;
-    width: 100%;
-    text-align: start;
-    margin: 0.5rem;
-    font-size: 20px;
-    color: var(--wht);
+  position: relative;
+  width: 100%;
+  text-align: start;
+  margin: 0.5rem;
+  font-size: 20px;
+  color: var(--wht);
 `;
 
 export const Title = styled.h2`
-    justify-content: flex-start;
-    font-weight: normal;
-    width: 100%;
+  justify-content: flex-start;
+  font-weight: normal;
+  width: 100%;
+  margin-bottom: 0.5em;
 `;
 export const Title2 = styled.h2`
-    margin: .5rem auto;
-    display: flex;
-    justify-content: center;
-    font-weight: normal;
-    width: 100%;
+  margin: 0.5rem auto;
+  display: flex;
+  justify-content: center;
+  font-weight: normal;
+  width: 100%;
 `;
 export const SubTitle = styled.h2`
-    justify-content: center;
-    text-align: center;
-    font-weight: normal;
-    width: 100%;
+  justify-content: center;
+  text-align: center;
+  font-weight: normal;
+  width: 100%;
 `;
 
 export const Image = styled.img`
-    width: 10rem;
-    height: 10rem;
-    margin: 1rem .5rem;
-    justify-content: flex-start;
-    border-radius: 100%;
+  width: 10rem;
+  height: 10rem;
+  margin: 1rem 0.5rem;
+  justify-content: flex-start;
+  border-radius: 100%;
 `;
-
